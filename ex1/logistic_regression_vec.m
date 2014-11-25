87aabb7144dbf65dc7e6423cae71520b6e5d7f59
civilstat <jerzywieczorek@gmail.com>
2014-11-25 12:52:26 -0500

    Switch to vectorized objective and gradient: 4 sec = 800x faster

--- ex1/logistic_regression_vec.m
+++ ex1/logistic_regression_vec.m
@@ -5,6 +5,7 @@ function [f,g] = logistic_regression_vec(theta, X,y)
   %   X - The examples stored in a matrix.  
   %       X(i,j) is the i'th coordinate of the j'th example.
   %   y - The label for each example.  y(j) is the j'th example's label.
+  %       y is a row vector.
   %
   m=size(X,2);
   
@@ -19,3 +20,12 @@ function [f,g] = logistic_regression_vec(theta, X,y)
   %        Store the objective function value in 'f', and the gradient in 'g'.
   %
 %%% YOUR CODE HERE %%%
+
+  h = 1./(1+exp(-theta'*X));
+  resid = h - y;
+  f = -(y*log(h)' + (1-y)*log(1-h)'); % penalty function f=J(theta)
+  g = X*resid'; % gradient g=J'(theta)
+
+  % This vectorized version took 4 sec to run on my laptop: 800x faster than looping!
+
+end
